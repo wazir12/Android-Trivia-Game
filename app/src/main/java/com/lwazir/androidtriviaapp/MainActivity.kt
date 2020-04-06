1f254e97ecfbbf8cb093fe203b227f0bb12c6898
Lalit <wazirlalit96420@gmail.com>
2020-04-07 00:01:06 +0530

    adding up back button logic

--- app/src/main/java/com/lwazir/androidtriviaapp/MainActivity.kt
+++ app/src/main/java/com/lwazir/androidtriviaapp/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.lwazir.androidtriviaapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,7 +13,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController)
 
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        var navConfig = this.findNavController(R.id.myNavHostFragment)
+        return navConfig.navigateUp()
     }
 
 
