14e6d99aa5be9bc6d469630bbbf1eaacd2da7782
Lalit <wazirlalit96420@gmail.com>
2020-04-07 21:11:56 +0530

    added navigation drawer menu with animation effection

--- app/src/main/java/com/lwazir/androidtriviaapp/GameWonFragment.kt
+++ app/src/main/java/com/lwazir/androidtriviaapp/GameWonFragment.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.Toast
-import androidx.core.app.ShareCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -45,9 +44,12 @@ class GameWonFragment : Fragment() {
     }
     private fun getShareIntent() : Intent{
         val args = GameWonFragmentArgs.fromBundle(arguments!!)
+
         val shareIntent : Intent = Intent(Intent.ACTION_SEND)
-        shareIntent.setType("text/String")
+        shareIntent.setType("text/plain")
             .putExtra(Intent.EXTRA_TEXT,getString(R.string.share_success_text, args.numCorrect,args.numQuestions))
+            .putExtra(Intent.EXTRA_EMAIL,"wazirlalit96420@gmail.com")
+
         return shareIntent
     }
 
